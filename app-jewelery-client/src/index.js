d975fc303a2b9f0ee368d496b6b7ab89a6ccf7d6
FeruzDev <feruzjalilov7314@gmail.com>
2021-07-28 14:20:04 +0300

    dsdsad

--- app-jewelery-client/src/index.js
+++ app-jewelery-client/src/index.js
@@ -9,7 +9,7 @@ import "./styles/global.scss";
 import {rootReducer} from "./redux/reducers/rootReducer";
 import thunk from "redux-thunk";
 
-const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
